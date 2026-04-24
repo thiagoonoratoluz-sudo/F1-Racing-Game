3e06b0b96dcdc4cc61ed7afc2fc69b3d332d52fa
thiagoonoratoluz-sudo <thiagoonoratoluz@gmail.com>
2026-04-23 23:34:19 -0300

    Add F1 drivers array to characters.gd

---
+++ scripts/characters.gd
@@ -0,0 +1,11 @@
+var f1Drivers = [
+    { name: "Ayrton Senna", helmetColor: "Yellow", characteristics: [{ skill: "Speed", value: 10 }, { skill: "Skill", value: 10 }, { skill: "Experience", value: 10 }] },
+    { name: "Alain Prost", helmetColor: "Blue", characteristics: [{ skill: "Speed", value: 9 }, { skill: "Skill", value: 9 }, { skill: "Experience", value: 10 }] },
+    { name: "Michael Schumacher", helmetColor: "Red", characteristics: [{ skill: "Speed", value: 10 }, { skill: "Skill", value: 10 }, { skill: "Experience", value: 10 }] },
+    { name: "Fernando Alonso", helmetColor: "Orange", characteristics: [{ skill: "Speed", value: 9 }, { skill: "Skill", value: 9 }, { skill: "Experience", value: 9 }] },
+    { name: "Felipe Massa", helmetColor: "Green", characteristics: [{ skill: "Speed", value: 8 }, { skill: "Skill", value: 8 }, { skill: "Experience", value: 7 }] },
+    { name: "Rubens Barrichello", helmetColor: "Brown", characteristics: [{ skill: "Speed", value: 7 }, { skill: "Skill", value: 8 }, { skill: "Experience", value: 8 }] },
+    { name: "Sebastian Vettel", helmetColor: "White", characteristics: [{ skill: "Speed", value: 10 }, { skill: "Skill", value: 10 }, { skill: "Experience", value: 8 }] }
+];
+
+export default f1Drivers;
